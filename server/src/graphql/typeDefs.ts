937ebef71a975713fd3a1ca51d5fa1d39dcc9a22
Rens Philipsen <rens.philipsen@oneshoe.nl>
2021-04-16 15:46:20 +0200

    Finish module 6

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -2,7 +2,7 @@ import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
   type Listing {
-    id: ID!
+    _id: ID!
     title: String!
     image: String!
     address: String!
