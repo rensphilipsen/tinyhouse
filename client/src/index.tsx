0a2022a32e2505c0bb33c60bb03343f17fe54268
Rens Philipsen <rens.philipsen@oneshoe.nl>
2021-05-14 09:06:17 +0200

    PT2: Progress module 2

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { Listings } from "./sections/Listings";
+import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/index.css";
 
 const client = new ApolloClient({ uri: "/api", cache: new InMemoryCache() });
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
 ReactDOM.render(
   <ApolloProvider client={client}>
     <React.StrictMode>
-      <Listings title="Tinyhouse listings" />
+      <App />
     </React.StrictMode>
   </ApolloProvider>,
   document.getElementById("root")
